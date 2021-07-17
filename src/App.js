2d92fae6c1b76e3cf438b650dcde79a20929178f
Teri Eyenike <teyenike1@gmail.com>
2021-07-17 12:52:20 +0100

    pricing table created with tailwind

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Card from './components/Card';
 
-function App() {
+import { plans } from './plans';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-full px-6 py-12 lg:flex lg:justify-center lg:items-center">
+      <div className="grid md:grid-cols-2 md:gap-0 lg:grid-cols-3 gap-12 lg:gap-0">
+        {React.Children.toArray(
+          plans.map((plan) => (
+            <div
+              className={`w-full max-w-md mx-auto ${
+                plan.featured
+                  ? 'order-first lg:order-none lg:scale-110 lg:transform lg:z-10'
+                  : 'lg:transform lg:scale-90'
+              }`}>
+              <Card {...plan} />
+            </div>
+          ))
+        )}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/plans.js
@@ -0,0 +1,35 @@
+export const plans = [
+  {
+    name: 'basic',
+    price: 19,
+    frequency: 'month',
+    featured: false,
+    features: [
+      'Nullam gragve reowqf',
+      'Mauris neque',
+      'Duis auctor tincidunt leo',
+    ],
+  },
+  {
+    name: 'premium',
+    price: 30,
+    frequency: 'month',
+    featured: true,
+    features: [
+      'Nullam gragve reowqf',
+      'Mauris neque',
+      'Duis auctor tincidunt leo',
+    ],
+  },
+  {
+    name: 'classic',
+    price: 22,
+    frequency: 'month',
+    featured: false,
+    features: [
+      'Nullam gragve reowqf',
+      'Mauris neque',
+      'Duis auctor tincidunt leo',
+    ],
+  },
+];
